b4a72ce378c1d1811ea3069a6d833c0219f68485
LUUTU JOSEPH <107415914+miirotrevor@users.noreply.github.com>
2022-10-27 16:34:18 +0300

    Add files via upload

---
+++ boolean.py
@@ -0,0 +1,22 @@
+# Declaring variables
+a = 10
+b = 20
+
+# Comparing variables
+print(a < b)
+print(a == b)
+print(a > b)
+# Python program to demonstrate
+# or operator
+
+a = 1
+b = 2
+c = 4
+
+if a > b or b < c:
+	print(True)
+else:
+	print(False)
+
+if a or b or c:
+	print("Atleast one number has boolean value as True")
---
+++ constructors.py
@@ -0,0 +1,16 @@
+class shelbygamers:
+
+	# default constructor
+	def __init__(self):
+		self.shelby = "shelbygamers"
+
+	# a method for printing data members
+	def print_shelby(self):
+		print(self.shelby)
+
+
+# creating object of the class
+obj = shelbygamers()
+
+# calling the instance method using the object obj
+obj.print_shelby()
